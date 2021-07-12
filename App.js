312d670badef6b1b051ab48b75346f1614930bc5
rachanon khongchai <6210110297@psu.ac.th>
2021-07-12 11:25:42 +0700

    3SA04 5. Router

--- App.js
+++ App.js
@@ -1,26 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
 import Weather from './components/Weather';
-import Constants  from 'expo-constants';
+import ZipCodeScreen from './screen/ZipCodeScreen';
+import Constants from 'expo-constants';
+
+const Stack = createStackNavigator()
 
 export default function App() {
-
-  const doIt = () => {
-    console.log("Hello from console")
-  }
-
   return (
-    <View style={styles.container}>
-      <Weather zipCode="90110" />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ZipCodeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-   
+
   },
 });
---
+++ screen/ZipCodeScreen.js
@@ -0,0 +1,47 @@
+import React,{useNavigation} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet,FlatList,View,Text } from 'react-native';
+
+const availableZipItems = [
+    { place: 'Hatyai', code: '90110' },
+    { place: 'Trang', code: '92000' },
+    { place: 'Chiangmai', code: '50000' },
+    { place: 'Khonkaen', code: '40000' },
+    { place: 'Chonburi', code: '20000' },
+   ]
+   const ZipItem = ({place, code, navigation}) => (
+    <View>
+    <Text>{place}</Text>
+    <Text>{code}</Text>
+    </View>
+    )
+   const _keyExtractor = item => item.code
+   export default function ZipCodeScreen(){
+    const navigation = useNavigation()
+    return (
+    <View>
+    <FlatList
+    data={availableZipItems}
+    keyExtractor={_keyExtractor}
+    renderItem={({item}) => <ZipItem {...item} navigation={navigation}/>}
+    />
+    <StatusBar style="auto" />
+    </View>
+    );
+    
+   }
+
+   const styles = StyleSheet.create({
+       zipItem:{
+           flex:1,
+           flexDirection:'row',
+           justifyContent: 'space-evenly',
+
+       },
+       zipPlace:{
+           flex:1,
+       },
+       zipCode:{
+            flex:1,
+       }
+   })
