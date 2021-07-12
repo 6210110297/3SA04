04822c43fd82c1d1f9fc94b23f4212795b86adcb
rachanon khongchai <6210110297@psu.ac.th>
2021-07-12 10:21:27 +0700

    3SA04 Flex Box

--- components/Weather.js
+++ components/Weather.js
@@ -1,31 +1,48 @@
-import React,{useState} from "react";
-import { StyleSheet,Text,ImageBackground,View  } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, ImageBackground, View } from "react-native";
 import Forecast from "./Forcast";
 
-export default function Weather(props) { 
-    const [forecastInfo, setForecastInfo]=useState({
-        main:'-',
-        description:'-',
-        temp:0
+export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
     })
 
     return (
         <View>
-        <ImageBackground source={require('../bg.png')} style={styles.backdrop}>
-        <Text>Zip Code</Text>
-        <Text>{props.zipCode}</Text>
-        <Forecast {...forecastInfo} />
-        </ImageBackground>
+            <ImageBackground source={require('../bg.png')} style={styles.backdrop}>
+                <View style={styles.container}>
+                    <Text style={styles.text}>Zip Code is {props.zipCode}</Text>
+                    
+                    <Forecast {...forecastInfo} />
+                </View>
+
+            </ImageBackground>
         </View>
     );
 
-    
-   }
-   const styles = StyleSheet.create({
+
+}
+const styles = StyleSheet.create({
     backdrop: {
-    alignItems: 'center',
-    width: '100%',
-    height: '100%'
+
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+        
     },
-   });
-   
+    container: {
+        padding: 15,
+        width:'100%',
+        alignItems:'center',
+        backgroundColor:'#000',
+        opacity: 0.5,
+        
+    },
+    text:{
+        fontSize:24,
+        color:'#fff',
+    }
+});
